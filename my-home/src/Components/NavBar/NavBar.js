cd349e454fd6b7f7ce40552ad8363079d55bf1ad
muhammadnoman710 <nomi32655@gmail.com>
2023-02-26 12:26:26 +0500

    navbar

---
+++ my-home/src/Components/NavBar/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './NavBar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
+function NavBar() {
+
+  return (
+    <div className='Navbar'>
+        <div className='NavInside'>
+        <img src="../Icons/Group 18.svg" alt='error'/>
+
+        <div className='ui icon input'>
+        <i className='search icon'></i>
+            <input className="prompt" type="text" placeholder='Search Item Here'/>
+        </div>
+        <Container> 
+            <Col sm={8}>
+                <text className='para1'>Explore</text>  {/*Change this line into button later*/}
+                <text className='para2'>My Items</text> {/*Change this line into button later*/}
+                <text className='para3'>Following</text> {/*Change this line into button later*/}
+            </Col>
+        </Container>
+        <div>
+        <button className='create'>Create</button>
+        <button className='connect'>Connect</button>
+        </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default NavBar
